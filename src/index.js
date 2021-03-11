45f9ff43c0e19d5bd0fa9dd0253b7b807743f38f
xopek-pro <xopek.pro@internet.ru>
2021-03-12 01:24:32 +0300

    feat: implement function that reverse digits of this number

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
-module.exports = function reverse (n) {
-  
+module.exports = 
+function reverse (n) {
+//   debugger;  
+    return Math.abs(n).toString().split("").reverse().join("");
 }
+
+// console.log(reverse(162));
